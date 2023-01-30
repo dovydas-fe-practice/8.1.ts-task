f5dff72cec31d94ce288717bd9d44cfe78b077db
Dovydas Miknys <Dovydas.Miknys@itechart-group.com>
2023-01-30 16:34:48 +0200

    task 5

--- task5.ts
+++ task5.ts
@@ -1,8 +1,11 @@
-type FIXME = any
-
 type OrderState = 'initial' | 'inWork' | 'buyingSupplies' | 'producing' | 'fullfilled'
+type ValidOrderStates = Exclude<OrderState, 'buyingSupplies' | 'producing'>
 
 // Hint: with type guards return type can be removed
-export const getUserOrderStates = (orderStates: OrderState[]): FIXME => {
-    return orderStates.filter((state) => state !== 'buyingSupplies' && state !== 'producing')
+export const getUserOrderStates = (orderStates: OrderState[]) => {
+    return orderStates.filter((state) => isValidState(state))
+}
+
+function isValidState(state: OrderState): state is ValidOrderStates {
+    return state !== 'buyingSupplies' && state !== 'producing'
 }
